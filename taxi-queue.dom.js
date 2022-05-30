22f478f2e5c73069d18eefaa4a77f56888713b05
Zeenat <zeenatavontuur@gmail.com>
2022-05-30 13:12:02 +0200

    first commit

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -2,10 +2,17 @@
 
 
 // DOM element references
-
+const addPassenger = document.querySelector(".join_queue");
+const removePassenger = document.querySelector(".leave_queue")
+const passengerCount = document.querySelector(".count")
 // create Factory Function instance
 
 const taxiQueue = TaxiQueue();
 
 // DOM events
-
+ addPassenger.addEventListener("click", function(){
+     alert('working');
+ });
+ removePassenger.addEventListener("click", function(){
+     alert("hmm")
+ });
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,31 +1,40 @@
 function TaxiQueue() {
 
+	let _queueLength = 0
+	let _taxiLength = 0
 
 	function joinQueue() {
-
+		_queueLength++
 	}
 
 	function leaveQueue() {
-
+		if (_queueLength > 0) {
+			_queueLength--
+		}
 	}
 
 	function joinTaxiQueue() {
-
+		_taxiLength++
 	}
 
 	function queueLength() {
-
+		return _queueLength
 	}
 
 	function taxiQueueLength() {
-
+		return _taxiLength
 	}
 
-	function taxiDepart(){
-
+	function taxiDepart() {
+		if (_queueLength >= 12) {
+			_queueLength-12
+		} else if (_queueLength < 12) {
+			_queueLength++
+		}
 	}
 
 	return {
+		queueLength,
 		joinQueue,
 		leaveQueue,
 		joinTaxiQueue,
